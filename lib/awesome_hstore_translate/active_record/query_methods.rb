b155ee855101f5d823b3690d032d0d742e3f53fd
Aaron Craigie <git@craigie.org.uk>
2018-06-03 12:39:44 +0100

    check to_sym available

--- lib/awesome_hstore_translate/active_record/query_methods.rb
+++ lib/awesome_hstore_translate/active_record/query_methods.rb
@@ -68,11 +68,11 @@ module AwesomeHstoreTranslate
       end
 
       def translated_attributes(opts)
-        opts.select{ |key, _| self.translated_attribute_names.include?(key.to_sym) }
+        opts.select{ |key, _| self.translated_attribute_names.include?(key.try(:to_sym) ? key.to_sym : key) }
       end
 
       def untranslated_attributes(opts)
-        opts.reject{ |key, _| self.translated_attribute_names.include?(key.to_sym) }
+        opts.reject{ |key, _| self.translated_attribute_names.include?(key.try(:to_sym) ? key.to_sym : key) }
       end
     end
   end
